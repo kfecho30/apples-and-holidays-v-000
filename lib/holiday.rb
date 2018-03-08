aff79f2f355a8d4e13a6bfb4b972c4bf5b2cda6b
Kelsey Fecho <kfecho30@gmail.com>
2018-03-08 21:43:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize}:"
+    puts "#{season.split("_").capitalize}:"
     holidays.each do |attribute, value|
       puts "  #{attribute.capitalize}: #{value.join(", ")}"
     end
